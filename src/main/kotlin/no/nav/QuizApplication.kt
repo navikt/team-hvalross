a0ebb68f2de63f99cee9791ce65ab860e95b4e02
Andrea Devold Fjeld <andrea.devold.fjeld@gmail.com>
2024-06-19 19:27:10 +0200

    Prøvde å løse arithmetic oppgave

--- src/main/kotlin/no/nav/QuizApplication.kt
+++ src/main/kotlin/no/nav/QuizApplication.kt
@@ -18,6 +18,7 @@ class QuizApplication(private val teamName: String): QuizParticipant(teamName) {
         logger.log(question)
         if (question.category == "team-registration") handleRegisterTeam(question)
         if (question.category == "arithmetic") arithmetic(question)
+        //if (question.category == "ping-pong") ping(question)
     }
 
 
@@ -39,13 +40,42 @@ class QuizApplication(private val teamName: String): QuizParticipant(teamName) {
     }
 
     private fun arithmetic(question: Question){
-        val x = 94/95
-        val roundedX = x.toInt()
-        answer(question.category, questionId="f1e5fa1d-b2af-4d5b-a37a-4f5e116ec848", roundedX.toString())
+        //println(question.question)
+        val msg = question.question
+        msg.split(" ")
+        val x = msg[0].toInt()
+        val y = msg[2].toInt()
+        val operation = msg[1]
+        val operations = arrayOf("+", "-", "/", "*")
+        if(operation.equals(operations[0])) {
+            val answer = x+y
+            answer(question.category, question.id(), answer.toInt().toString())
+
+        }
+        else if(operation.equals(operations[1])) {
+                val answer = x-y
+                answer(question.category, question.id(), answer.toInt().toString())
+
+        }
+        else if(operation.equals(operations[2])) {
+            val answer = x/y   
+            answer(question.category, question.id(), answer.toInt().toString())
+        }
+        else if(operation.equals(operations[3])) {
+            val answer = x*y
+            answer(question.category, question.id(), answer.toInt().toString())
+        }
+        
     }
+ }
     /*
+    [Question] category: ping-pong, question: ping, id: fabd13f6-52a3-438a-944a-d20caa057f40
+    
+    private fun ping(question: Question){
+        answer(question.category, questionId="fabd13f6-52a3-438a-944a-d20caa057f40", "pong")
+    }
+    
     [Question] category: arithmetic, question: 
     94 / 95 (svaret må rundes til int), id: f1e5fa1d-b2af-4d5b-a37a-4f5e116ec848
     */
 
-}
